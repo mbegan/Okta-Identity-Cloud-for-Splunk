316890a7ad08cc3047f248305ddca69f755986c3
Matt Egan <mbegan@gmail.com>
2017-10-21 17:44:23 -0600

    now officially flailing

--- bin/input_module_okta_identity_cloud.py
+++ bin/input_module_okta_identity_cloud.py
@@ -172,13 +172,17 @@ def _okta_caller(helper, resource, params, method, limit):
         max_log_batch = 6000
         helper.log_debug(log_metric + "_okta_caller has a default max_log_batch value of: " + (str(max_log_batch)))
 
+    helper.log_debug(log_metric + "_okta_caller is about to do some fetch empty pages stuff)
+    
     try:
         fetchEmptyPages = bool(helper.get_global_setting('fetch_empty_pages'))
         helper.log_debug(log_metric + "_okta_caller has defined fetch_empty_pages value of: " + (str(fetch_empty_pages)))
     except:
         fetchEmptyPages = bool(False)
         helper.log_debug(log_metric + "_okta_caller has default fetch_empty_pages value of: " + (str(fetch_empty_pages)))
-    
+
+    helper.log_debug(log_metric + "_okta_caller has just done some fetch empty pages stuff)
+
     myCon = True
     while ((n_val.startswith(myValidPattern)) and (myCon)):
         helper.log_debug(log_metric + "_okta_caller fetching next page: " + n_val)
